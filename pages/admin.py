71865a96b60ee45ec90647818ebf7b12cac85dff
Santiago <storoor@eafit.edu.co>
2023-09-19 07:19:02 -0500

    cambios en el modelo vehiculos electricos

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserAdmin, Route, ChargePoint
+from .models import UserAdmin, Route, ChargePoint,ElectricVehicle
 # Register your models here.
 
 admin.site.register(UserAdmin)
 admin.site.register(Route)
 admin.site.register(ChargePoint)
+admin.site.register(ElectricVehicle)
