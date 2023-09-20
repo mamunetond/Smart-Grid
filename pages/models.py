e1dd0ada9a735edd8dd8904c074b6825f86e5911
mamunetond <47578828+mamunetond@users.noreply.github.com>
2023-09-19 22:24:59 -0500

    LLaves foráneas configuradas

--- pages/models.py
+++ pages/models.py
@@ -1,19 +1,8 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class UserAdmin(models.Model):
-    identification = models.IntegerField()
-    name = models.CharField(max_length=255)
-    mail = models.EmailField(max_length=255)
-    phone = PhoneNumberField(null=False, blank=False, unique=True)
-    created_at_user = models.DateTimeField(auto_now=True)
-    updated_at_user = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.identification
-
 
 class Route(models.Model):
     tittle = models.CharField(max_length=255)
@@ -25,6 +14,7 @@ class Route(models.Model):
     longitude_origin = models.FloatField()
     longitude_destination = models.FloatField()
     battery_percentage = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     created_at_route = models.DateTimeField(auto_now_add=True)
     updated_at_route = models.DateTimeField(auto_now_add=True)
     
@@ -37,6 +27,7 @@ class ChargePoint(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     activate = models.BooleanField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     created_at_point = models.DateTimeField(auto_now_add=True)
     updated_at_point = models.DateTimeField(auto_now_add=True)
     
@@ -53,8 +44,25 @@ class ElectricVehicle(models.Model):
     battery_capacity_kwh = models.DecimalField(max_digits=5, decimal_places=2)#The capacity of the battery in kilowatt-hours.
     range_kilometers = models.PositiveIntegerField()#The range of the vehicle in kilometers.
     charging_time_hours = models.DecimalField(max_digits=5, decimal_places=2)#The charging time in hours.
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     created_at_electricVehicle = models.DateTimeField(auto_now_add=True)
     updated_at_electricVehicle = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
+    
+    
+
+class UserAdmin(models.Model):
+    identification = models.IntegerField()
+    name = models.CharField(max_length=255)
+    mail = models.EmailField(max_length=255)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    route = models.ForeignKey(Route,on_delete=models.CASCADE)
+    charge_point = models.ForeignKey(ChargePoint,on_delete=models.CASCADE)
+    electric_vehicle = models.ForeignKey(ElectricVehicle,on_delete=models.CASCADE)
+    created_at_user = models.DateTimeField(auto_now=True)
+    updated_at_user = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.identification
 
