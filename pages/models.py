daff0a68a407b7a6a858632022ed887355766e44
mamunetond <47578828+mamunetond@users.noreply.github.com>
2023-11-21 14:33:49 -0500

    api_Trip done

--- pages/models.py
+++ pages/models.py
@@ -71,29 +71,34 @@ class UserAdmin(models.Model):
     
 class Trip(models.Model):
     vehiculo_id = models.CharField(max_length=100)
-    ruta1_id = models.CharField(max_length=100)
-    ruta2_id = models.CharField(max_length=100)
-    ruta3_id = models.CharField(max_length=100)
-    distancia1 = models.IntegerField()
-    distancia2 = models.IntegerField()
-    distancia3 = models.IntegerField()
-    duracion1 = models.IntegerField()
-    duracion2 = models.IntegerField()
-    duracion3 = models.IntegerField()
-    consumo_ajustado1 = models.IntegerField()
-    consumo_ajustado2 = models.IntegerField()
-    consumo_ajustado3 = models.IntegerField()
-    porcentaje_bateria1 = models.IntegerField()
-    porcentaje_bateria2 = models.IntegerField()
-    porcentaje_bateria3 = models.IntegerField()
+    ruta_id = models.CharField(max_length=100)
+    distancia = models.IntegerField()
+    duracion = models.IntegerField()
+    consumo_ajustado = models.IntegerField()
+    porcentaje_bateria = models.IntegerField()
     #rutas = models.JSONField()  # Almacena un arreglo de rutas
     rutas = [
-        (ruta1_id,distancia1,duracion1,consumo_ajustado1,porcentaje_bateria1),
-        (ruta2_id,distancia2,duracion1,consumo_ajustado2,porcentaje_bateria2),
-        (ruta3_id,distancia3,duracion3,consumo_ajustado3,porcentaje_bateria3),
+        (ruta_id,distancia,duracion,consumo_ajustado,porcentaje_bateria),
     ]
     origen = models.CharField(max_length=200)
     destino = models.CharField(max_length=200)
+    Statistics_id = models.IntegerField()
+    fecha = models.DateField()
+    Enero = models.IntegerField()
+    Febrero = models.IntegerField()
+    Marzo = models.IntegerField()
+    Abril = models.IntegerField()
+    Mayo = models.IntegerField()
+    Junio = models.IntegerField()
+    Julio = models.IntegerField()
+    Agosto = models.IntegerField()
+    Septiembre = models.IntegerField()
+    Octubre = models.IntegerField()
+    Noviembre = models.IntegerField()
+    Diciembre = models.IntegerField()
+    porcentaje_consumido = models.IntegerField()
+    carga_restante = models.IntegerField()
+    consumo_total = models.IntegerField()
 
     def _str_(self):
         return f"Viaje desde {self.origen} hasta {self.destino} para el vehículo {self.vehiculo_id}"
