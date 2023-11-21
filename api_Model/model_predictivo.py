d1bd3bd0efd06fe973a26561a0ea12821be031d1
Yhilmar <yachaverrc@eafit.edu.co>
2023-11-21 00:10:27 -0500

    rutas completas

--- api_Model/model_predictivo.py
+++ api_Model/model_predictivo.py
@@ -1,3 +1,5 @@
+# model_predictivo.py
+
 import os
 import json
 from dotenv import load_dotenv
@@ -52,13 +54,20 @@ def encontrar_ruta_optima(origen, destino, porcentaje_bateria_actual):
 
         mensaje_ruta = "Con el porcentaje de batería actual, puedes llegar al destino." if porcentaje_bateria_necesario <= porcentaje_bateria_actual else f"Necesitas cargar aproximadamente {max(0, porcentaje_bateria_necesario - porcentaje_bateria_actual):.2f}% más de batería para usar esta ruta."
 
+        # Ajusta la información de la ruta para incluir solo lo necesario
+        informacion_ruta = {
+            "overview_polyline": ruta_info.get("overview_polyline", {}).get("points", ""),
+            "bounds": ruta_info.get("bounds", {})
+        }
+
         ruta_actual = {
             "numero_ruta": i + 1,
             "distancia": ruta_info['legs'][0]['distance']['text'],
             "duracion": ruta_info['legs'][0]['duration']['text'],
             "consumo_ajustado": consumo_ajustado,
             "porcentaje_bateria_necesario": porcentaje_bateria_necesario,
-            "mensaje": mensaje_ruta
+            "mensaje": mensaje_ruta,
+            "informacion_ruta": informacion_ruta  # Incluye la información de la ruta en cada ruta
         }
 
         resultados["rutas"].append(ruta_actual)
--- api_Model/serializers.py
+++ api_Model/serializers.py
@@ -1,13 +1,22 @@
 # serializers.py
-
 from rest_framework import serializers
 
-class RutasOptimasSerializer(serializers.Serializer):
-    rutas = serializers.ListField(child=serializers.DictField())
-    ruta_optima = serializers.DictField()
+class RutaSerializer(serializers.Serializer):
+    numero_ruta = serializers.IntegerField()
+    distancia = serializers.CharField()
+    duracion = serializers.CharField()
+    consumo_ajustado = serializers.FloatField()
+    porcentaje_bateria_necesario = serializers.FloatField()
+    mensaje = serializers.CharField()
+    informacion_ruta = serializers.JSONField()
 
-    def to_representation(self, instance):
-        return {
-            'rutas': instance['rutas'],
-            'ruta_optima': instance.get('ruta_optima', None)
-        }
+class RutasOptimasSerializer(serializers.Serializer):
+    rutas = RutaSerializer(many=True)
+    ruta_optima = serializers.JSONField()
+
+    def get_ruta_optima(self, obj):
+        ruta_optima = obj.get('ruta_optima', None)
+        if ruta_optima:
+            # Incluye la información de la ruta en la ruta óptima
+            ruta_optima["informacion_ruta"] = obj.get('informacion_ruta', {})
+        return ruta_optima
